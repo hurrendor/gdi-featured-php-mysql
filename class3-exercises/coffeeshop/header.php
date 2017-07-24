bc07f439b9d1186f07df8da4c1fd7caaaf6ad9e3
Stacy <hurrendor@gmail.com>
2017-07-23 20:34:41 -0500

    Return title to original

--- class3-exercises/coffeeshop/header.php
+++ class3-exercises/coffeeshop/header.php
@@ -1,19 +1,19 @@
 <?php
 // Report all PHP errors
 error_reporting(-1);
-$site_title;
+$site_title = "Wake Up!";
 ?>
 
 <!doctype html>
 <html>
 <head>
-	<title><?php echo site_title; ?></title>
+	<title><?php echo $site_title; ?></title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 	<div class="container">
 		<div class="header">
-			<h1><?php echo site_title; ?></h1>
+			<h1><?php echo $site_title; ?></h1>
 			<h2>A Site About Coffee</h2>
 		</div><!-- .header -->
 		<div class="main">
